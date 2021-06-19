b6407e58611fecd1d3fd922b0e4f6978d6c5b862
rsa16 <rehanaliportmanhelper.com@gmail.com>
2021-06-19 18:02:34 -0400

    Added support for episode links

--- app.py
+++ app.py
@@ -86,6 +86,7 @@ class Episode(Resource):
 if __name__ == '__main__':
     api.add_resource(SearchResults, '/search')
     api.add_resource(AnimeInfo, '/anime')
+    api.add_resource(Episode, '/video')
     port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', port=port)
 
